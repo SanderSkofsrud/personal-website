b444f4e591acb36fa131903839fabcaba6342227
sande <sander@skofsrud.net>
2023-06-15 11:20:27 +0200

    Test pipeline

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -8,7 +8,6 @@ export const Projects = forwardRef<HTMLDivElement>((props, ref) => {
         <li>Project Name, Description, Link to GitHub Repo</li>
         <li>Project Name, Description, Link to GitHub Repo</li>
         <li>Project Name, Description, Link to GitHub Repo</li>
-        <li>Project Name, Description, Link to GitHub Repo</li>
       </ul>
     </div>
   );
